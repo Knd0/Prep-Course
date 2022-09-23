7110d3df7f666e4ffa59564a231e9b91679d5c66
Franco D <luafranco09@hotmail.com>
2022-09-23 15:56:55 -0300

    Test

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -99,7 +99,8 @@ function mayorQueCincuenta(num) {
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  
+  var resto = (x % y);
+  return resto
 }
 
 function esPar(num) {
